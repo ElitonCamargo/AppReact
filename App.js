b94ec71ad944779db33383165c040354cb4b3b38
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-03-21 22:23:14 -0300

    My firts commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>AppReact</Text>
+      <Text>Criado por Eliton</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ff55aa',
+    alignItems: 'baseline',
+    justifyContent: 'space-around',
   },
 });
